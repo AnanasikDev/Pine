30d4298173713542d4d73b105aca208f4ddfb120
Олег Бровченков <oleg.brovchenkov@gmail.com>
2021-08-01 10:54:01 +0300

    Init; Tasks added

---
+++ Pine/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pine
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task t = new Task("test ", new Vector2(0, 60), false);
+            foreach (Control c in t.FormsElements)
+            {
+                this.Controls.Add(c);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Pine/Task.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows.Forms;
+namespace Pine
+{
+    public struct Task
+    {
+        public string Title;
+        public bool IsDone;
+
+        public Vector2 Position;
+
+        public Control[] FormsElements;
+
+        public Task(string title, Vector2 position, bool isDone)
+        {
+            Title = title;
+            Position = position;
+            IsDone = isDone;
+
+            CheckBox checkBox = new CheckBox();
+            checkBox.SetPosition(position + Locations.CheckBoxPosition);
+            
+            checkBox.Checked = false;
+            checkBox.Enabled = true;
+            checkBox.Name = Title;
+            checkBox.Text = Title;
+            checkBox.AutoSize = true;
+
+            FormsElements = new Control[1] { checkBox };
+        }
+    }
+    public static class Locations
+    {
+        public static readonly Vector2 CheckBoxPosition = Vector2.zero;
+        public static readonly Vector2 TitlePosition = new Vector2(100, 0);
+    }
+    public struct Vector2
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public static Vector2 zero { get { return new Vector2(0, 0); } }
+
+        public Vector2(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public static Vector2 operator + (Vector2 a, Vector2 b)
+        {
+            return new Vector2(a.x + b.x, a.y + b.y);
+        }
+    }
+    public static class Extensions
+    {
+        public static Vector2 GetPosition (this Control element)
+        {
+            return new Vector2(element.Bounds.X + element.Width / 2, element.Bounds.Y - element.Height / 2);
+        }
+        public static void SetPosition(this Control element, Vector2 position)
+        {
+            Vector2 size = new Vector2(element.Width, element.Height);
+
+            element.Bounds = 
+                new System.Drawing.Rectangle(
+                    position.x + size.x / 2, 
+                    position.y - size.y / 2,
+
+                    size.x,
+                    size.y);
+        }
+    }
+}
